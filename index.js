acc782b560e21d0c7ef3788ead750591edf77894
JulietteMadi <juliette.madinier@gmail.com>
2022-12-12 10:03:03 +0100

    first commit

--- index.js
+++ index.js
@@ -1,53 +1,69 @@
 const formSubmit = document.getElementById("submitButton");
 const form = document.querySelector("form");
 const elements = form.elements;
-const notFirstTrial = false;
+const today = new Date();
 
-/* const toastTrigger = document.getElementById("submitButton");
-const toastEnd = document.getElementById("successToast");
-if (toastTrigger) {
-    toastTrigger.addEventListener('submit', () => {
-        const toast = new bootstrap.Toast(toastEnd)
-        toast.show();
-    })
-} */
-
-function formValidation() {
+function submitValidation() {
     for (let element of elements) {
+        const helpText = document.getElementById(`${element.id}Help`)
+        const tooltip = new bootstrap.Tooltip(element)
         if (element.validity.valid) {
             element.classList.remove("is-invalid")
             element.classList.add("is-valid")
+            tooltip.disable();
         } else {
             element.classList.remove("is-valid")
             element.classList.add("is-invalid");
             event.preventDefault();
-            const tooltip = new bootstrap.Tooltip(element, { boundary: document.body })
-            onmouseover = (event) => { tooltip.show(); };
+            helpText.classList.add("text-danger")
+            onmouseenter = (event) => { tooltip.show(); };
         }
     }
 };
 
-function inputValidation() {
+
+function onChangeValidation() {
     for (let element of elements) {
+        const helpText = document.getElementById(`${element.id}Help`)
+        const tooltip = new bootstrap.Tooltip(element)
         element.addEventListener('change', event => {
             if (element.validity.valid) {
                 element.classList.remove("is-invalid")
                 element.classList.add("is-valid")
+                helpText.classList.remove("text-danger")
+                tooltip.disable();
             } else {
                 element.classList.remove("is-valid")
                 element.classList.add("is-invalid");
                 event.preventDefault();
-                const tooltip = new bootstrap.Tooltip(element, { boundary: document.body })
+                helpText.classList.add("text-danger")
+                tooltip.enable();
                 onmouseover = (event) => { tooltip.show(); };
+                console.log("went at the onmouseover")
             }
         })
     }
 }
 
+function focusInput() {
+    for (let element of elements) {
+        if (!element.validity.valid) {
+            element.focus();
+            break;
+        }
+    }
+}
 
+formSubmit.addEventListener('click', submitValidation);
+formSubmit.addEventListener('click', onChangeValidation);
+formSubmit.addEventListener('click', focusInput);
 
-formSubmit.addEventListener('click', formValidation);
-formSubmit.addEventListener('click', inputValidation);
-
-
+const toastTrigger = document.getElementById("submitButton");
+const toastEnd = document.getElementById("successToast");
+if (toastTrigger) {
+    formSubmit.addEventListener('click', () => {
+        const toast = new bootstrap.Toast(toastEnd)
+        toast.show();
+    })
+}
 

96374f2b9483fd421d069f5eaa68be3d00a14f57
JulietteMadi <juliette.madinier@gmail.com>
2022-12-13 14:45:09 +0100

    JS refactored

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const submitForm = document.getElementById("submitButton")
 const form = document.querySelector("form");
 const elements = form.elements;
 const now = new Date();
@@ -7,7 +6,7 @@ const toastEnd = document.getElementById("successToast");
 
 
 /* Date Validation */
-const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
+const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
 document.getElementById("date").setAttribute("min", today);
 
 
@@ -23,40 +22,63 @@ form.addEventListener("submit", (event) => {
 });
 
 
+
 /* Form behavior to handle valid & invalid input */
 for (let element of elements) {
+
     type = element.type;
     if (type != "submit") {
-        const tooltip = new bootstrap.Tooltip(element, { title: "Veuillez renseigner ce champ" });
+
         const helpText = document.getElementById(`${element.id}Help`);
-        element.addEventListener("invalid", (event) => {
-            event.preventDefault();
+
+        function elementValid() {
+            element.classList.remove("is-invalid");
+            element.classList.add("is-valid");
+            helpText.classList.remove("text-danger");
+            helpText.classList.add("text-success")
+            tooltip.disable();
+        }
+
+        function elementInvalid() {
+            element.classList.remove("is-valid");
             element.classList.add("is-invalid");
             helpText.classList.add("text-danger");
-        })
+            helpText.classList.remove("text-success")
+            tooltip.enable();
+        }
+
+        function invalidFocus() {
+            const invalidInput = form.querySelector(':invalid')
+            invalidInput.focus();
+        }
+
+        const tooltip = new bootstrap.Tooltip(element, { title: "Veuillez renseigner ce champ" });
+        function tooltipMessage() {
+            if (element.validity.rangeUnderflow && element.id == "date") {
+                tooltip.setContent({ '.tooltip-inner': 'Doit être égale ou supérieure à aujourd\'hui' });
+            } else if (element.validity.rangeUnderflow && element.id == "price") {
+                tooltip.setContent({ '.tooltip-inner': 'Doit être positif' });
+            };
+        }
+
+
+        element.addEventListener("invalid", (event) => {
+            event.preventDefault();
+            elementInvalid();
+            tooltipMessage();
+        });
 
         element.addEventListener('change', (event) => {
             event.preventDefault();
-            let invalidInput = form.querySelector(':invalid')
-            invalidInput.focus();
+            invalidFocus();
             if (element.validity.valid) {
-                element.classList.remove("is-invalid");
-                element.classList.add("is-valid");
-                helpText.classList.remove("text-danger");
-                tooltip.disable();
+                elementValid();
             } else {
-                element.classList.remove("is-valid");
-                element.classList.add("is-invalid");
-                helpText.classList.add("text-danger");
-                tooltip.enable();
-                if (element.validity.rangeUnderflow && element.id == "date") {
-                    tooltip.setContent({ '.tooltip-inner': 'Doit être égale ou supérieure à aujourd\'hui' })
-                } else if (element.validity.rangeUnderflow && element.id == "price") {
-                    tooltip.setContent({ '.tooltip-inner': 'Doit être positif' });
-                }
-            }
-        })
-    }
+                elementInvalid();
+                tooltipMessage();
+            };
+        });
+    };
 };
 
 

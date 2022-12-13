7cfca1dafd5d41003262af4057d5792963a5394a
JulietteMadi <juliette.madinier@gmail.com>
2022-12-13 10:08:48 +0100

    Messages on specific mistakes and on success

--- index.js
+++ index.js
@@ -3,49 +3,51 @@ const elements = form.elements;
 var now = new Date();
 const toastEnd = document.getElementById("successToast");
 
+const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
+document.getElementById("date").setAttribute("min", today);
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const toast = new bootstrap.Toast(toastEnd)
     toast.show();
-    document.documentElement.scrollTop = 0;
     form.reset();
     for (let element of elements) {
-        element.classList.remove("is-valid");
+        element.classList.remove("is-valid")
     }
 });
 
 for (let element of elements) {
-    const tooltip = new bootstrap.Tooltip(element)
-    const helpText = document.getElementById(`${element.id}Help`)
-    element.addEventListener("invalid", (event) => {
-        event.preventDefault();
-        type = element.type;
-        if (type != "submit") {
+    type = element.type;
+    if (type != "submit") {
+        const tooltip = new bootstrap.Tooltip(element, { title: "Veuillez renseigner ce champ" });
+        const helpText = document.getElementById(`${element.id}Help`);
+        element.addEventListener("invalid", (event) => {
+            event.preventDefault();
             element.classList.add("is-invalid");
-            helpText.classList.add("text-danger")
-        }
-    })
-    element.addEventListener('change', (event) => {
-        event.preventDefault();
-        type = element.type;
-        if (type != "submit") {
-            if (element.validity) {
-                element.classList.remove("is-invalid")
-                element.classList.add("is-valid")
-                helpText.classList.remove("text-danger")
+            helpText.classList.add("text-danger");
+        })
+        element.addEventListener('change', (event) => {
+            event.preventDefault();
+            if (element.validity.valid) {
+                element.classList.remove("is-invalid");
+                element.classList.add("is-valid");
+                helpText.classList.remove("text-danger");
                 tooltip.disable();
             } else {
-                element.classList.remove("is-valid")
+                element.classList.remove("is-valid");
                 element.classList.add("is-invalid");
-                helpText.classList.add("text-danger")
+                helpText.classList.add("text-danger");
                 tooltip.enable();
-                onmouseover = (event) => { tooltip.show() };
+                onmouseenter = (event) => { tooltip.show() };
+                if (element.validity.rangeUnderflow && element.id == "date") {
+                    tooltip.setContent({ '.tooltip-inner': 'Doit être égale ou supérieure à aujourd\'hui' })
+                } else if (element.validity.rangeUnderflow && element.id == "price") {
+                    tooltip.setContent({ '.tooltip-inner': 'Doit être positif' })
+                }
             }
-        }
-    })
-}
+        })
+    };
+};
 
-const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
-document.getElementById("date").setAttribute("min", today);
+
 

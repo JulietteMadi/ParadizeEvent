01a4d7f8b94249262fa764eb35d38d83bb89356a
JulietteMadi <juliette.madinier@gmail.com>
2022-12-13 12:51:53 +0100

    Added the focus on invalid input

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
+const submitForm = document.getElementById("submitButton")
 const form = document.querySelector("form");
 const elements = form.elements;
-var now = new Date();
+const now = new Date();
 const toastEnd = document.getElementById("successToast");
 
+
+
+/* Date Validation */
 const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
 document.getElementById("date").setAttribute("min", today);
 
+
+/* Form behavior when submit successfully */
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const toast = new bootstrap.Toast(toastEnd)
+    const toast = new bootstrap.Toast(toastEnd);
     toast.show();
     form.reset();
     for (let element of elements) {
-        element.classList.remove("is-valid")
+        element.classList.remove("is-valid");
     }
 });
 
+
+/* Form behavior to handle valid & invalid input */
 for (let element of elements) {
     type = element.type;
     if (type != "submit") {
@@ -26,8 +34,11 @@ for (let element of elements) {
             element.classList.add("is-invalid");
             helpText.classList.add("text-danger");
         })
+
         element.addEventListener('change', (event) => {
             event.preventDefault();
+            let invalidInput = form.querySelector(':invalid')
+            invalidInput.focus();
             if (element.validity.valid) {
                 element.classList.remove("is-invalid");
                 element.classList.add("is-valid");
@@ -38,15 +49,14 @@ for (let element of elements) {
                 element.classList.add("is-invalid");
                 helpText.classList.add("text-danger");
                 tooltip.enable();
-                onmouseenter = (event) => { tooltip.show() };
                 if (element.validity.rangeUnderflow && element.id == "date") {
                     tooltip.setContent({ '.tooltip-inner': 'Doit être égale ou supérieure à aujourd\'hui' })
                 } else if (element.validity.rangeUnderflow && element.id == "price") {
-                    tooltip.setContent({ '.tooltip-inner': 'Doit être positif' })
+                    tooltip.setContent({ '.tooltip-inner': 'Doit être positif' });
                 }
             }
         })
-    };
+    }
 };
 
 

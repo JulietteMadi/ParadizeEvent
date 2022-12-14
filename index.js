c433acf27f1ac27d7eb6d9d06a9f3f154d81c886
JulietteMadi <juliette.madinier@gmail.com>
2022-12-14 12:29:43 +0100

    Tooltips dynamic messages + tooltips appears on invalid only

--- index.js
+++ index.js
@@ -3,11 +3,37 @@ const elements = form.elements;
 const now = new Date();
 const toastEnd = document.getElementById("successToast");
 
-
-
 /* Date Validation */
 const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
 document.getElementById("date").setAttribute("min", today);
+
+function elementValidStyle(element, helpText) {
+    element.classList.remove("is-invalid");
+    element.classList.add("is-valid");
+    helpText.classList.remove("text-danger");
+    helpText.classList.add("text-success");
+}
+
+function elementInvalidStyle(input, helpText) {
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
+    helpText.classList.add("text-danger");
+    helpText.classList.remove("text-success");
+}
+
+function tooltipMessage(element, tooltip) {
+    if (element.validity.rangeUnderflow && element.id == "date") {
+        tooltip.setContent({ '.tooltip-inner': 'Doit être égale ou supérieure à aujourd\'hui' });
+    } else if (element.validity.rangeUnderflow && element.id == "price") {
+        tooltip.setContent({ '.tooltip-inner': 'Doit être positif' });
+    };
+}
+
+
+function focusOnInvalid() {
+    const invalidInput = form.querySelector(':invalid')
+    invalidInput.focus();
+}
 
 
 /* Form behavior when submit successfully */
@@ -17,10 +43,13 @@ form.addEventListener("submit", (event) => {
     toast.show();
     form.reset();
     for (let element of elements) {
-        element.classList.remove("is-valid");
+        const helpText = document.getElementById(`${element.id}Help`);
+        element.classList.remove('is-invalid');
+        element.classList.remove('is-valid');
+        helpText.classList.remove("text-danger");
+        helpText.classList.remove("text-success")
     }
 });
-
 
 
 /* Form behavior to handle valid & invalid input */
@@ -28,55 +57,29 @@ for (let element of elements) {
 
     type = element.type;
     if (type != "submit") {
-
         const helpText = document.getElementById(`${element.id}Help`);
-
-        function elementValid() {
-            element.classList.remove("is-invalid");
-            element.classList.add("is-valid");
-            helpText.classList.remove("text-danger");
-            helpText.classList.add("text-success")
-            tooltip.disable();
-        }
-
-        function elementInvalid() {
-            element.classList.remove("is-valid");
-            element.classList.add("is-invalid");
-            helpText.classList.add("text-danger");
-            helpText.classList.remove("text-success")
-            tooltip.enable();
-        }
-
-        function invalidFocus() {
-            const invalidInput = form.querySelector(':invalid')
-            invalidInput.focus();
-        }
-
         const tooltip = new bootstrap.Tooltip(element, { title: "Veuillez renseigner ce champ" });
-        function tooltipMessage() {
-            if (element.validity.rangeUnderflow && element.id == "date") {
-                tooltip.setContent({ '.tooltip-inner': 'Doit être égale ou supérieure à aujourd\'hui' });
-            } else if (element.validity.rangeUnderflow && element.id == "price") {
-                tooltip.setContent({ '.tooltip-inner': 'Doit être positif' });
-            };
-        }
+        tooltip.disable();
 
         element.addEventListener("invalid", (event) => {
             event.preventDefault();
-            elementInvalid();
-            tooltipMessage();
-            invalidFocus();
+            elementInvalidStyle(element, helpText);
+            focusOnInvalid();
         });
 
         element.addEventListener('change', (event) => {
             event.preventDefault();
-            invalidFocus();
             if (element.validity.valid) {
-                elementValid();
+                elementValidStyle(element, helpText);
+                tooltip.disable();
             } else {
-                elementInvalid();
-                tooltipMessage();
+                elementInvalidStyle(element, helpText);
             };
+            if (element.className.includes("is-invalid")) {
+                tooltip.enable();
+                tooltipMessage(element, tooltip);
+            }
+
         });
     };
 };

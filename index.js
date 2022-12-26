90a7297c03fbdab6e15f1070ad50f4a2902ed9b5
JulietteMadi <juliette.madinier@gmail.com>
2022-12-26 12:49:28 +0100

    Implemented GetOrCreateInstance method

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const form = document.querySelector("form");
 const elements = form.elements;
-const now = new Date();
 const toastEnd = document.getElementById("successToast");
+const now = new Date();
+
 
 /* Date Validation */
-const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
-document.getElementById("date").setAttribute("min", today);
+const today = now.toISOString();
+document.getElementById("date").setAttribute("min", today.slice(0, 10));
 
-function elementValidStyle(element, helpText) {
+function elementValidStyle(element) {
+    const helpText = document.getElementById(`${element.id}Help`);
     element.classList.remove("is-invalid");
     element.classList.add("is-valid");
     helpText.classList.remove("text-danger");
     helpText.classList.add("text-success");
 }
 
-function elementInvalidStyle(input, helpText) {
-    input.classList.remove("is-valid");
-    input.classList.add("is-invalid");
+function elementInvalidStyle(element) {
+    const helpText = document.getElementById(`${element.id}Help`);
+    element.classList.remove("is-valid");
+    element.classList.add("is-invalid");
     helpText.classList.add("text-danger");
     helpText.classList.remove("text-success");
 }
 
 function tooltipMessage(element, tooltip) {
-    if (element.validity.rangeUnderflow && element.id == "date") {
-        tooltip.setContent({ '.tooltip-inner': 'Doit être égale ou supérieure à aujourd\'hui' });
-    } else if (element.validity.rangeUnderflow && element.id == "price") {
-        tooltip.setContent({ '.tooltip-inner': 'Doit être positif' });
-    }
-}
+    if (!(element.validity.valueMissing))
+        if (element.validity.rangeUnderflow && element.id == "date") {
+            tooltip.setContent({ '.tooltip-inner': 'Doit être égale ou supérieure à aujourd\'hui' });
+        } else if (element.validity.rangeUnderflow && element.id == "price") {
+            tooltip.setContent({ '.tooltip-inner': 'Doit être positif' });
+        } else {
+            tooltip.setContent({ '.toolip-inner': 'Ce champ est obligatoire' })
+        };
 
+}
 
 function focusOnInvalid() {
     const invalidInput = form.querySelector(':invalid')
@@ -39,6 +45,7 @@ function focusOnInvalid() {
 /* Form behavior when submit successfully */
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    console.log("has been submitted")
     const toast = new bootstrap.Toast(toastEnd);
     toast.show();
     form.reset();
@@ -57,29 +64,26 @@ for (let element of elements) {
 
     type = element.type;
     if (type != "submit") {
-        const helpText = document.getElementById(`${element.id}Help`);
-        const tooltip = new bootstrap.Tooltip(element, { title: "Veuillez renseigner ce champ" });
-        tooltip.disable();
-
         element.addEventListener("invalid", (event) => {
             event.preventDefault();
-            elementInvalidStyle(element, helpText);
+            tooltip = bootstrap.Tooltip.getOrCreateInstance(element);
+            elementInvalidStyle(element);
             focusOnInvalid();
             tooltip.enable();
         });
 
         element.addEventListener('change', (event) => {
             event.preventDefault();
+            tooltip = bootstrap.Tooltip.getOrCreateInstance(element);
             if (element.validity.valid) {
-                elementValidStyle(element, helpText);
+                elementValidStyle(element);
+                tooltip.hide();
                 tooltip.disable();
             } else {
-                elementInvalidStyle(element, helpText);
-            };
-            if (element.className.includes("is-invalid")) {
+                elementInvalidStyle(element);
                 tooltip.enable();
                 tooltipMessage(element, tooltip);
-            }
+            };
         });
     };
 };
